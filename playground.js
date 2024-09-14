40a8ecd68c15835d0163fdacec74f3af4a5229a2
xe on <angecdmt@gmail.com>
2024-09-14 21:08:11 +0000

    Lesson 2

--- playground.js
+++ playground.js
@@ -48,13 +48,46 @@ var camper = "James";
 var camper = "David";   //var keyword can overwrite variable declarations
 console.log(camper);
 
-let camper = "James";
-let camper = "David";
+//let camper = "James";
+//let camper = "David";
 //console.log(camper);  //error
-//let was introduced in ES6. it cannot overwrite variable declarations or rather a variable with the same name can only be declared once.
+/*let was introduced in ES6. it cannot overwrite variable declarations or rather a variable with the same name can only be declared once.*/
 
 const FAV_PET = "Cats";
-FAV_PET = "Dogs";
+//FAV_PET = "Dogs"; //error
 //can also declare variables using 'const' keyword. It is similar to 'let' but 'const' are read-only
 //uppercase variable identifiers for immutable values
 //lowercase or camelCase for mutable values (objects and arrays)
+
+/*
+let myVar = 5 + 10;
+let myVar = 12 - 6;
+let myVar = 13 * 13;
+let myVar = 16 / 2;
+*/
+
+i++;    //equivalent of i = i + 1
+i--;    //i = i - 1;
+
+let multiplyFN = 3.14 * 4.9;
+let divideFN = 8.658 / 2.463;
+console.log(multiplyFN);
+console.log(divideFN);
+
+console.log(5 % 2); //% - outputs the remainder
+
+let addVal = 1;
+addVal += 5;
+console.log(addVal); //6
+
+let subtractVal = 5;
+subtractVal -= 2
+console.log(subtractVal);
+
+let multiplyVal = 4;
+multiplyVal *= 2;
+console.log(multiplyVal);
+
+let divideVal = 10;
+divideVal /= 2;
+console.log(divideVal);

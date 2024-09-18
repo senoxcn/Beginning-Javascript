d37ed22fd4ee357a0abfb5c3aa1dc60b19952790
Xeon <angecdmt@gmail.com>
2024-09-19 00:56:44 +0800

    Lesson 1

---
+++ playground2.js
@@ -0,0 +1,22 @@
+const sandwich = ["peanut butter", "jelly", "bread"];
+const teams = [["Bulls", 23], ["White Sox", 45]];   //multidimensional array
+
+const array = [50, 60, 70];
+console.log(array[0]);
+const data = array[1];
+console.log(data);
+
+const ourArray = [50, 40, 30];
+ourArray[0] = 15;
+console.log(ourArray);
+
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14]
+];
+
+const subarray = arr[3];            //[[10, 11, 12], 13, 14]
+const nestedSubarray = arr[3][0];   //[10, 11, 12]
+const element = arr[3][0][1];       //11
